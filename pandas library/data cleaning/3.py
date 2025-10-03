9cfca3440cd7f427de0326a2fbc8b8e3288d03ce
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-10-03 17:14:11 +0530

    modified:   pandas library/data cleaning/3.py

--- pandas library/data cleaning/3.py
+++ pandas library/data cleaning/3.py
@@ -2,3 +2,7 @@
 
 #Wrong Data means Not to be "Empty cell" or 'Wrong format data' 
 import pandas as pd
+
+view=pd.read_csv("/home/yogesh/Downloads/data.csv")
+view.drop_duplicates(inplace=True)
+print(view.to_string())
