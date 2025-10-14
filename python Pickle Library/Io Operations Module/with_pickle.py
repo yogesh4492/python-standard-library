fa6d94d4fca24c71935e9f7550336430468c5bbc
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-10-14 18:26:32 +0530

    deleted:    python Pickle Library/Io Operations Module/file 	modified:   python Pickle Library/Io Operations Module/with_pickle.py

--- python Pickle Library/Io Operations Module/with_pickle.py
+++ python Pickle Library/Io Operations Module/with_pickle.py
@@ -4,17 +4,17 @@ import time
 data={'key':1,'name':'yogesh','age':20}
 start_time=time.time()
 
-with open ("file",'wb') as wp:
-    pickle.dump(data,wp)
-serialization_time=time.time()-start_time
+# with open ("file.pickle",'wb') as wp:
+#     pickle.dump(data,wp)
+# serialization_time=time.time()-start_time
 
 start_time1=time.time()
-with open("file",'rb') as rp:
+with open("file.pickle",'rb') as rp:
     d=pickle.load(rp)
 desir_time=time.time()-start_time1
 
 print(d)
-print(f"{serialization_time:.10f}")
+# print(f"{serialization_time:.10f}")
 print(f"{desir_time:.10f}")
 
 
