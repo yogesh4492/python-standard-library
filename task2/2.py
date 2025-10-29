975c4b8b416f944e58f7d5aa0c80f643cf9ee3cb
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-10-29 18:32:59 +0530

    modified:   task2/2.py 	new file:   task2/j.csv

--- task2/2.py
+++ task2/2.py
@@ -10,6 +10,7 @@ app=typer.Typer()
 
 def load_json(file):
     with open(file,'r',encoding="utf-8") as j:
+        # print(file)
         return json.load(j)
 
 # @app.command()
@@ -17,31 +18,36 @@ def main(file,field,output):
     # files=glob.glob(f"{inp}/**/*.json",recursive=True)
     # print(files)
     # print(len(files))
-    # field=fields.split(",")
-    print(file)
+    field=field.split(",")
+    # print(file)
     with open(output,"w",newline="") as c:
         csw=csv.DictWriter(c,fieldnames=field)
         csw.writeheader()
-        for j in file:
-            segment=load_json(j)
-            # print(list(segment.keys())[0])
-            if list(segment.keys())[0]=='documentId':
-                # print(segment['segments'][0]['segmentId'])
-                for j in segment['segments']:
-                    row={k:j[k] for k in j if k in field}
-                    # row1={k:j[k] for k in j if k not in field}
-                    row['audioId']=segment['audioId']
-                    row['audioFileNamePath']=segment['audioFileNamePath']
-                    csw.writerow(row)
-                    
-            else:
-                for j in segment['value']['segments']:
-                    row={k:j[k] for k in j if k in field}
-                    row['content']=j['transcriptionData'].get('content')
-                    row['audioId']="None"
-                    row['audioFileNamePath']='None'
-                    csw.writerow(row)
-                    # row={k:j[k] for k in j if k in field}
+    
+        segment=load_json(file)
+        # # print(segment)
+
+        # for i in segment:
+        #     print(i)
+        # # # print(list(segment.keys())[0])
+        # if list(segment.keys())[0]=='documentId':
+            # print(segment['segments'][0]['segmentId'])
+        if 'value' not in segment:
+            for j in segment['segments']:
+                row={k:j[k] for k in j if k in field}
+                # row1={k:j[k] for k in j if k not in field}
+                row['audioId']=segment['audioId']
+                row['audioFileNamePath']=segment['audioFileNamePath']
+                csw.writerow(row)
+                
+        else:
+            for j in segment['value']['segments']:
+                row={k:j[k] for k in j if k in field}
+                row['content']=j['transcriptionData'].get('content')
+                row['audioId']="None"
+                row['audioFileNamePath']='None'
+                csw.writerow(row)
+                # row={k:j[k] for k in j if k in field}
                     # # row1={k:j[k] for k in j if k not in field}
                     # row['audioId']=segment['audioId']
                     # row['audioFileNamePath']=segment['audioFileNamePath']
@@ -53,13 +59,13 @@ def up(input,fields,output):
     files=glob.glob(f"{input}/**/*.json",recursive=True)
     # print(files)
     # print(len(files))
-    field=fields.split(",")
+    # field=fields.split(",")
     with Progress() as p:
         tab=p.add_task("Proceesing...",total=len(files))
         with ThreadPoolExecutor(max_workers=8) as e:
-            e.submit(main,files,field,output),total=len(files)
-            
-            
+            future={e.submit(main,file,fields,output)for file in files}
+            for i in future:
+                p.update(tab,advance=1)
             
     
 
